fb0e18d28a8e144e2688ce6db61b39b3eb924bdd
hanapulsar <217953127+hanapulsar@users.noreply.github.com>
2026-03-22 00:43:50 +0900

    Add frequency monobit test

--- lab_2/task/nist_tests.py
+++ lab_2/task/nist_tests.py
@@ -1,13 +1,28 @@
 import argparse
+import math
 
 
 def parse_arguments() -> argparse.Namespace:
     """
-    Adds and parses command-line arguments
+    Adds and parses command-line arguments.
     """
     parser = argparse.ArgumentParser(description="Template.")
     parser.add_argument('--Placeholder', '-p', default='Placeholder.txt', help='Placeholder')
     return parser.parse_args()
+
+
+def frequency_test(binary):
+    """
+    Frequency monobit test.
+    Used to evaluate the randomness of binary sequences.
+    :param binary: binary sequences to check.
+    :return: evaluated P-value.
+    """
+    n = len(binary)
+    n_sum = sum(1 if b == '1' else -1 for b in binary)
+    s_n = abs(n_sum) / math.sqrt(n)
+    p_value = math.erfc(s_n / math.sqrt(2))
+    return p_value
 
 
 def main() -> None:
