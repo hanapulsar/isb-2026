c78354b41f27e52664e212616e046190433b6b2e
hanapulsar <217953127+hanapulsar@users.noreply.github.com>
2026-03-22 07:46:18 +0900

    Add Longest Run of Ones in a Block test

--- lab_2/task/nist_tests.py
+++ lab_2/task/nist_tests.py
@@ -1,6 +1,8 @@
 import argparse
 import math
 
+from scipy.special import gammaincc
+
 
 def parse_arguments() -> argparse.Namespace:
     """
@@ -11,7 +13,7 @@ def parse_arguments() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def frequency_test(binary):
+def frequency_test(binary: str) -> float:
     """
     Frequency monobit test.
     Used to evaluate the randomness of binary sequences.
@@ -25,7 +27,7 @@ def frequency_test(binary):
     return p_value
 
 
-def run_test(binary):
+def runs_test(binary: str) -> float:
     """
     Runs test.
     The purpose of the runs test is to determine whether the number of runs
@@ -45,6 +47,43 @@ def run_test(binary):
             v_n += 1
 
     p_value = math.erfc(abs(v_n - 2 * n * zeta * (1 - zeta)) / (2 * math.sqrt(2 * n) * zeta * (1 - zeta)))
+    return p_value
+
+
+def longest_run_ones_in_block_test(binary: str) -> float:
+    """
+    Test for the Longest Run of Ones in a Block.
+    The purpose of this test is to determine whether the length of the longest
+    run of ones within the tested sequence is consistent with the length of
+    the longest run of ones that would be expected in a random sequence.
+    :param binary: binary sequences to check.
+    :return: evaluated P-value.
+    """
+    n = len(binary)
+    m = 8
+
+    pi_probs = [0.2148, 0.3672, 0.2305, 0.1875]
+    v_values = [0, 0, 0, 0]
+
+    blocks = [binary[i:i + m] for i in range(0, n, m)]
+
+    for block in blocks:
+        max_sequence = max(len(run) for run in block.split('0'))
+
+        if max_sequence <= 1:
+            v_values[0] += 1
+        elif max_sequence == 2:
+            v_values[1] += 1
+        elif max_sequence == 3:
+            v_values[2] += 1
+        else:
+            v_values[3] += 1
+
+    chi_sq = 0.0
+    for i in range(4):
+        chi_sq += ((v_values[i] - 16 * pi_probs[i]) ** 2) / (16 * pi_probs[i])
+
+    p_value = gammaincc(1.5, chi_sq / 2)
     return p_value
 
 
